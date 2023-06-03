daa0b3e3237d5008c2d712043f2bbb92ff18c97e
Maciej Szadkowski <chx100148@student.chorzow.wsb.pl>
2023-06-03 19:10:38 +0200

    Zmiana 03.06.2023

---
+++ Zadania 03.06.2023/10_ostatnia_cyfra_liczby_naturalnej.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int a, b, c;
+        Console.WriteLine("Podaj liczbe naturalną");
+        a = inputValue();
+        if (a > 0)
+        {
+            b = a;
+            c = a % 10;
+            Console.WriteLine($"Ostatnia cyfra liczby {b} to {c}");
+        } else
+        {
+            Console.WriteLine("Podana liczba nie jest liczbą naturalną");
+        }
+    }
+    private static int inputValue()
+    {
+        int result;
+        while (!int.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
