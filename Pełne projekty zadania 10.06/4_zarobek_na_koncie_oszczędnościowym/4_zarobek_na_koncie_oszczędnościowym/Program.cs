ceff966c09499fc69fc17bdcfb605f3aec243910
Maciej Szadkowski <chx100148@student.chorzow.wsb.pl>
2023-06-03 19:58:24 +0200

    Zmiana

---
+++ Pełne projekty zadania 10.06/4_zarobek_na_koncie_oszczędnościowym/4_zarobek_na_koncie_oszczędnościowym/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class MyProgram
+{
+    static void Main()
+    {
+        decimal kp, opr, kz;
+        int lm;
+        Console.WriteLine("Podaj kwotę początkową");
+        kp = inputValue();
+        Console.WriteLine("Podaj oprocentowanie w skali roku");
+        opr = inputValue();
+        Console.WriteLine("Podaj liczbę miesięcy oszczędzania");
+        lm = int.Parse(Console.ReadLine());
+
+        kz = Zarobek(kp, opr, lm);
+
+        Console.WriteLine($"Kwota zarobiona (z uwzględnieniem podatku Belki) wynosi {kz}");
+    }
+
+    static decimal Zarobek(decimal kp, decimal opr, int lm)
+    {
+        decimal opm = opr / 12 / 100;
+        decimal kz = kp * (decimal)Math.Pow(1 + (double)opm, lm);
+
+        kz = kz - (decimal)(0.19 * (double)(kz - kp));
+
+        return kz;
+    }
+
+    private static decimal inputValue()
+    {
+        decimal result;
+        while (!decimal.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
